3cebd55df9f87202b5a9f48c024feddc95b01a36
dbadocha <dawidbadocha@gmail.com>
2020-07-30 22:28:48 +0200

    Started with documentation

--- infolab-XY/XYstring.h
+++ infolab-XY/XYstring.h
@@ -2,14 +2,26 @@
 
 typedef unsigned long long ull;
 
+/**
+*
+*/
 class XYstring
 {
 public:
 	XYstring();
 	~XYstring();
+	/**
+	*
+	*/
 	char find(ull pos);
 
 private:
+	/**
+	*
+	*/
 	ull iterationPivot(ull pos);
+	/**
+	*
+	*/
 	int iterationsParity(ull pos);
 };
